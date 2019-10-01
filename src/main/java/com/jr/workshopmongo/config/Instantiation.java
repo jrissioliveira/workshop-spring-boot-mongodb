2316189f3a304932cd3f6fea5b820efd21ba010b
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-10-01 20:44:32 -0300

    Referencing User Posts

--- src/main/java/com/jr/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jr/workshopmongo/config/Instantiation.java
@@ -36,12 +36,14 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
-		
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abroços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2019"), "Bom dia", "Arcordei feliz hoje", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 	
 	
